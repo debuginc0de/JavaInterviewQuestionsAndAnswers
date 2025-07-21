bd6086ba4f8bbc31e116084487fed66b0ef43935
debuginc0de <rd09536@gmail.com>
2025-07-21 21:13:29 +0530

    init commit

--- JavaInterviewQuestionsAndAnswers/src/main/java/com/java/io/JavaInterviewQuestionsAndAnswers/Objects/Constructor/Animal.java
+++ JavaInterviewQuestionsAndAnswers/src/main/java/com/java/io/JavaInterviewQuestionsAndAnswers/Objects/Constructor/Animal.java
@@ -2,12 +2,20 @@ package com.java.io.JavaInterviewQuestionsAndAnswers.Objects.Constructor;
 
 class Animal {
     private String name;
+    private int legs;
 
     //constructor
-    public Animal(String name) {
+    public Animal(String name, int legs) {
         this.name = name;
+        this.legs = legs;
         System.out.println("Animal Constructor with name");
 
+    }
+
+    //Copy constructor
+    public Animal(Animal animal) {
+        this.name = animal.name;
+        this.legs = animal.legs;
     }
 
     //getter->It also a method which is return the name as per constructor
@@ -15,18 +23,21 @@ class Animal {
         return this.name;
     }
 
+    public int getLegs() {
+        return this.legs;
+    }
+
     static class Dog extends Animal {
 
         //parameterized constructor
-        public Dog(String name) {
-            super(name);
+        public Dog(String name, int legs) {
+            super(name, legs);
             //System.out.println("exit");
         }
 
         //Non-parameterized/default constructor
-        public Dog() {
-            super("Default Dog Name");
-            //System.out.println("Default");
-        }
+        //public Dog() {
+        //super("Default Dog Name and Legs");
+        //System.out.println("Default");
     }
 }
--- JavaInterviewQuestionsAndAnswers/src/main/java/com/java/io/JavaInterviewQuestionsAndAnswers/Objects/Constructor/ConstructorsExample.java
+++ JavaInterviewQuestionsAndAnswers/src/main/java/com/java/io/JavaInterviewQuestionsAndAnswers/Objects/Constructor/ConstructorsExample.java
@@ -3,10 +3,15 @@ package com.java.io.JavaInterviewQuestionsAndAnswers.Objects.Constructor;
 
 public class ConstructorsExample {
     public static void main(String[] args) {
-        //Animal animal=new Animal("Cat");
+        /*Animal animal=new Animal("Cat");
         Animal.Dog dog=new Animal.Dog();
         dog.getName();
-        //animal.getName();
+        animal.getName();
+        */
+        Animal animal=new Animal("cats",4);
+        Animal animal2=new Animal(animal);
+        System.out.println(animal2.getName());
+        System.out.println(animal2.getLegs());
 
     }
 }
